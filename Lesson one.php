b59f4334a2212d22258db2ee2fea2401e5521657
uzzol24 <uzzalbanik.freelancer@gmail.com>
2023-12-24 22:00:23 +0600

    One

---
+++ Lesson one.php
@@ -0,0 +1,29 @@
+<?php
+ $phone = "I phone";
+
+
+ switch($phone){
+
+    case'samsung':
+        echo "Best mobile phone is" .  $phone;
+    break;
+
+    case'Tecno':
+        echo "Best mobile phone is" .  $phone;
+    break;
+
+    case'nokia':
+        echo "Best mobile phone is" .  $phone;
+    break;
+
+    case'I phone':
+        echo "Best mobile phone is" .  $phone;
+    break;
+
+        default:
+           echo "I love it I phone";
+        break;
+
+ }
+
+?>
